2fc32b5c1c8928e0e67847a23c86dcf64581542e
Kaique Jacinto <79776257+KaiqueJacinto@users.noreply.github.com>
2021-08-05 17:52:08 -0300

    Update clique.py

--- cliquerapido/clique.py
+++ cliquerapido/clique.py
@@ -3,8 +3,8 @@ import win32api, win32con
 
 def clica(x,y):
     win32api.SetCursorPos((x,y)) # Coloca o mouse na posição desejada
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) # Aperta o botão direito do mouse para baixo
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) # Solta o botão direito
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) # Aperta o botão esquerdo do mouse para baixo
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) # Solta o botão esquerdo
     time.sleep(0.05) # Espera um pouco para não bugar o clique, esse tempo foi o mais rapido que consegui
     # Se o autoshot começar a cometer erros aumente o tempo de sleep
     '''Dependendo do modo você pode colocar 0.04 no sleep'''
